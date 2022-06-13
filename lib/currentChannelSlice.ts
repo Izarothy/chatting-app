4b078ae2453136d8e2bcbaa7894193d7c0527169
Izarothy <izarothmail@gmail.com>
2022-06-13 23:54:42 +0200

    Add types to the slice, add a selector hook

--- lib/currentChannelSlice.ts
+++ lib/currentChannelSlice.ts
@@ -1,17 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppState } from '../redux/store';
 
 export const currentChannelSlice = createSlice({
   name: 'currentChannel',
   initialState: {
-    value: "general",
+    value: 'general',
   },
   reducers: {
-    changeCurrentChannel: (state, action) => {
-      state.value = action.payload
-    }
+    changeCurrentChannel: (state, action: PayloadAction<string>) => {
+      state.value = action.payload;
+    },
   },
 });
 
 export const { changeCurrentChannel } = currentChannelSlice.actions;
+
+export const currentChannel = (state: AppState) => state.currentChannel.value;
 
 export default currentChannelSlice.reducer;
