192c0531b44d0c81e4b2db2be4ce2ed6c0b07c05
Izarothy <izarothmail@gmail.com>
2022-06-23 21:18:08 +0200

    Cover the null case in typeguards

--- lib/typeguards/channelTypeGuard.ts
+++ lib/typeguards/channelTypeGuard.ts
@@ -1,6 +1,7 @@
 import { ChannelT } from 'types/Types';
 
 const channelsTypeGuard = (valToCheck: any): valToCheck is ChannelT => {
+  if (valToCheck === null) return false;
   if (typeof valToCheck.isText !== 'boolean') return false;
 
   return true;
--- lib/typeguards/memberTypeGuard.ts
+++ lib/typeguards/memberTypeGuard.ts
@@ -1,6 +1,7 @@
 import { MemberT } from 'types/Types';
 
 const memberTypeGuard = (valToCheck: any): valToCheck is MemberT => {
+  if (valToCheck === null) return false;
   if (typeof valToCheck.content === 'string') return false;
   if (typeof valToCheck.isText === 'boolean') return false;
 
